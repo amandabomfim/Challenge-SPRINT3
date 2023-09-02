808c764436b40133d9a5dadbc6347e901884ddde
amandabomfim <rm99741@fiap.com.br>
2023-09-02 17:30:50 -0300

    Criado os componentes da pagina principal.

---
+++ src/components/ComponenteLogo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LogoPorto from '../images/logo2.png'
+
+export default function ComponenteLogo() {
+  return (
+    <div className="componente-logo">
+      <img src={LogoPorto} alt="Logotipo da Porto" />
+    </div>
+  );
+}
+
+
+
