79b10055ebccd51ff4e9c488d1ce69d37ceb6edb
amandabomfim <rm99741@fiap.com.br>
2023-09-02 17:33:11 -0300

    Estilizando o css.

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//Bloco de criação das rotas
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import PaginaInicial from './routes/PaginaInicial.jsx';
+
+
+const router = createBrowserRouter(
+  [
+    {path: '/',  element : <App />,
+    errorElement: <Error/>,
+    children:[
+        { path: '/', element : <PaginaInicial/>},
+      
+      ]
+  }
+ ]
+)
+
+//Bloco de criação das rotas
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
