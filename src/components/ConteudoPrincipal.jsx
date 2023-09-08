71b2921956af42ba1ee60d2a6985de7678bbb838
oleoz1n <leopity0@gmail.com>
2023-09-08 13:43:53 -0300

    Modificando estrutura da página inicial

--- src/components/ConteudoPrincipal.jsx
+++ src/components/ConteudoPrincipal.jsx
@@ -4,17 +4,19 @@ export default function ConteudoPrincipal() {
   return (
     <div className="conteudoPrincipal">
         
-        <p>Imagine ter uma experiência de verificação de bicicleta que é rápida, precisa e completamente automatizada. </p>
-
-        <p>Bem-vindo ao nosso sistema de vistoria da PORTO SEGURO!</p>
-        <p>Com apenas alguns cliques, nossa plataforma automatizada fará uma análise abrangente de sua bicicleta, verificando cada componente crítico para garantir que ela esteja em perfeitas condições de funcionamento. </p>
-
-        <p>Nossos algoritmos inteligentes identificam até mesmo as menores irregularidades, garantindo que você esteja sempre seguro e em pleno desempenho. Além disso, você pode realizar sua vistoria inicial a qualquer momento, em qualquer lugar.
-        </p>
-
-        <h2>Experimente agora com comodidade e precisão:</h2>
+        <h1>Seguro de bicicleta</h1>
+        <div className='textLeft-PI'>
+        <h2>Com o seguro você terá:</h2>
+        <ul>
+        <li>Proteção contra roubos</li>
+        <li>Cobertura contra danos</li>
+        <li>Assistência técnica e muito mais</li>
+        </ul>
+        </div>
+        <div className='botoes-pagina-inicial'>
         <button>Iniciar Vistoria</button> 
         <button>Equipe</button> 
+        </div>
     </div>
   );
 }
