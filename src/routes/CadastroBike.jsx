825a58d3ae6dd467b5a86c7aad057e2af9852780
amandabomfim <rm99741@fiap.com.br>
2023-09-12 18:27:51 -0300

    Criado e estilizado formulario para cadastro bike.

--- src/routes/CadastroBike.jsx
+++ src/routes/CadastroBike.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ComponenteLogo from '../components/ComponenteLogo';
+import ComponenteCadastroBike from '../components/ComponenteCadastroBike'
+import "../styles/CadastroBike.css"
+
 export default function CadastroBike() {
     return (
         <>
@@ -8,6 +11,9 @@ export default function CadastroBike() {
         <ComponenteLogo/>
         </div>
         <div className='titulo'>CADASTRO BIKE</div>
+        <div>
+            <ComponenteCadastroBike/>
+        </div>
         <div className="LinksNav">
             <Link to="/cadastro">Anterior</Link>
             <Link to="/cadastro/upload">Próximo</Link>
