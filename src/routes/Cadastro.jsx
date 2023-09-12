8f9c1d9ba42aa3f5821815855175e7d0e3247304
MatheusPinhoOliveira <matheuspinho736@gmail.com>
2023-09-12 19:03:10 -0300

    Adicionado css para a pagina Cadastro.

--- src/routes/Cadastro.jsx
+++ src/routes/Cadastro.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import ComponenteLogo from '../components/ComponenteLogo';
-
+import "../styles/Cadastro"
 
 
 export default function Cadastro() {
