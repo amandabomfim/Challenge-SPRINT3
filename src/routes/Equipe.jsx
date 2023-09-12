7fda9010bbceeef182b36d1e7ae630170103e480
esterdsilva <rm97798@fiap.com.br>
2023-09-11 21:14:07 -0300

    equipe

--- src/routes/Equipe.jsx
+++ src/routes/Equipe.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import ComponenteLogo from '../components/ComponenteLogo';
+import React from "react";
+import { Link } from "react-router-dom";
+import ComponenteLogo from "../components/ComponenteLogo";
 
 export default function Equipe() {
   return (
     <>
-      <div className='logo'>
-      <ComponenteLogo/>
+      <div className="logo">
+        <ComponenteLogo />
       </div>
-      <div className='titulo'>EQUIPE</div>
+      <div className="titulo">EQUIPE</div>
       <p>Equipe</p>
+      <h1>
+        99741 - Amanda Bomfim de Oliveira 
+        97798 - Ester Dutra da Silva 
+        550981 -Henrique Gerson Costa Correia 
+        99738 - Leonardo Dos Santos Guerra 
+        99004 -Maria Eduarda Ferreira da Mata 
+        551216 - Matheus Pinho Oliveira
+      </h1>
       <Link to="/">Voltar</Link>
-      </>
-    
-  )
+    </>
+  );
 }
